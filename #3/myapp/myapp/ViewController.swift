1891133741bb06aebd99317482721d62a0df8638
pawisoon <pawi552@gmail.com>
2017-03-18 16:06:00 +0000

    #3 files added

---
+++ #3/myapp/myapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by Pawel Szydlowski on 18/03/2017.
+//  Copyright © 2017 Pawel Szydlowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
